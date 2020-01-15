b86c5e763e410480668c1809ac5210d27b8be905
Jim Filippou <jimfilippou8@gmail.com>
2020-01-15 18:02:29 +0200

    Initial Commit

---
+++ hci-project-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hci-project-ios
+//
+//  Created by Jim Filippou on 15/1/20.
+//  Copyright © 2020 Jim Filippou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
